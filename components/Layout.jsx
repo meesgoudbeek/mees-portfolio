82e605d735446b1f53c90f47e7603a204c8377ff
Mees Goudbeek <meesgoudbeek@protonmail.com>
2024-08-09 13:47:46 +0200

    fix: removed themeswitcher for now

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import classNames from 'clsx'
 import Menu from '@/components/Menu'
-import ThemeSwitcher from '@/components/ThemeSwitcher'
 import fonts from '@/styles/fonts'
 
 const Layout = (props) => {
   const { children } = props
-  const [theme, setTheme] = React.useState({})
 
   return (
     <div
       className={classNames(
         'dark relative flex min-h-screen w-full justify-center bg-black font-sans lg:px-4 xl:px-8',
-        theme,
         ...fonts
       )}
     >
-      <ThemeSwitcher setTheme={setTheme} />
       <div className="fixed bottom-0 left-0 z-50 hidden w-full bg-black md:block lg:h-4 xl:h-8"></div>
       <div className="fixed top-0 left-0 z-50 hidden w-full bg-black md:block lg:h-4 xl:h-8"></div>
       <div className="relative w-full max-w-screen-xl">
